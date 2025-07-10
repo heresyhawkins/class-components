54e2db7f1d5acf42749cac797fbcfb9724f4912c
heresyhawkins <heresyhawkins@gmail.com>
2025-07-10 19:18:30 +0300

    feat: view list info

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,28 +1,41 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { SearchInput } from '../SearchInput/SearchInput';
 import { SearchButton } from '../SearchButton/SearchButton';
 import './Form.css';
+interface INamedApiResource {
+  name: string;
+  url: string;
+}
 
-interface INamedApiResourse {
+interface INamedApiResourceList {
   count: number;
   next: string | null;
   previous: string | null;
-  results: { url: string }[];
+  results: INamedApiResource[];
 }
 
 export const Form: FC = () => {
-  const [data, setData] = useState<{ url: string }[]>([]);
+  const [data, setData] = useState<INamedApiResource[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const fetchData = async (url = 'https://pokeapi.co/api/v2/ability') => {
     try {
       const response = await fetch(url);
-      const result = (await response.json()) as INamedApiResourse;
+      const result = (await response.json()) as INamedApiResourceList;
 
       setData(result.results);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    fetchData().catch((error) => {
+      console.error(error);
+    });
+  }, []);
 
   console.log(data, fetchData);
 
@@ -33,7 +46,20 @@ export const Form: FC = () => {
           <SearchInput placeholder="Write Something" />
           <SearchButton textContent="Search" />
         </div>
-        <div className="results-form"></div>
+        <div className="results-form">
+          <h2>Results:</h2>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul>
+              {data.map((item, index) => (
+                <li key={index}>
+                  <strong>{item.name}</strong>: {item.url}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </form>
     </>
   );
