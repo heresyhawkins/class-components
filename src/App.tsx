983b50425ca8486e54905aa5b2ef9398de0f0ef2
heresyhawkins <heresyhawkins@gmail.com>
2025-07-13 22:21:40 +0300

    feat: add styling and run prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import './App.css';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 import { Form } from './components/Form/Form';
 
 function App() {
   return (
     <>
-      <Form />
+      <ErrorBoundary>
+        <ErrorTriggerButton />
+        <Form />
+      </ErrorBoundary>
     </>
   );
 }
+const ErrorTriggerButton = () => {
+  const throwError = () => {
+    throw new Error('Test error thrown intentionally!');
+  };
+
+  return (
+    <div>
+      <button onClick={throwError}>Throw Error</button>
+    </div>
+  );
+};
 
 export default App;
