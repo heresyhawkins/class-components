06b847a85fdc113473d4c944bdaf493078ee94b5
heresyhawkins <heresyhawkins@gmail.com>
2025-08-03 13:32:17 +0300

    fix: delete unnecessary import

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { screen, fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import Form from './Form';
 import { renderWithProviders } from '../../utils/test-utils';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
