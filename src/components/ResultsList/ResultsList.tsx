9baf4cef9099f21715276d261f72482e8f9d2028
heresyhawkins <heresyhawkins@gmail.com>
2025-08-10 19:19:48 +0300

    feat: delete unnecessary info and call hooks

--- src/components/ResultsList/ResultsList.tsx
+++ src/components/ResultsList/ResultsList.tsx
@@ -20,36 +20,49 @@ export default function ResultsList({ pokemons, onPokemonClick }: ResultsListPro
 
   return (
     <ul className="pokemon-list">
-      {pokemons.map((pokemon) => (
-        <li key={pokemon.name} className="pokemon-card">
-          <div className="pokemon-checkbox">
-            <input
-              type="checkbox"
-              id={`select-${pokemon.name}`}
-              checked={selectedNames.has(pokemon.name)}
-              onChange={() =>
-                dispatch(
-                  togglePokemon({
-                    name: pokemon.name,
-                    url: pokemon.url,
-                  })
-                )
-              }
-            />
-            <label htmlFor={`select-${pokemon.name}`}></label>
-          </div>
+      {pokemons.map((pokemon) => {
+        const id = pokemon.url.split('/').filter(Boolean).pop() ?? '';
 
-          <div
-            className="pokemon-card-content"
-            onClick={() => onPokemonClick?.(pokemon.name)}
-            role="button"
-            tabIndex={0}
-          >
-            <h3 className="pokemon-name">{pokemon.name}</h3>
-            <p className="pokemon-loading">Details available on click</p>
-          </div>
-        </li>
-      ))}
+        return (
+          <li key={pokemon.name} className="pokemon-card">
+            <div className="pokemon-checkbox">
+              <input
+                type="checkbox"
+                id={`select-${pokemon.name}`}
+                checked={selectedNames.has(pokemon.name)}
+                onChange={() =>
+                  dispatch(
+                    togglePokemon({
+                      name: pokemon.name,
+                      url: pokemon.url,
+                    })
+                  )
+                }
+              />
+              <label htmlFor={`select-${pokemon.name}`}></label>
+            </div>
+
+            <div
+              className="pokemon-card-content"
+              onClick={() => onPokemonClick?.(pokemon.name)}
+              role="button"
+              tabIndex={0}
+            >
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                alt={pokemon.name}
+                className="pokemon-image"
+                onError={(e) => {
+                  (e.target as HTMLImageElement).src = 'https://via.placeholder.com/100';
+                }}
+              />
+              <h3 className="pokemon-name">{pokemon.name}</h3>
+              <div className="pokemon-id">#{id}</div>
+              <div className="pokemon-loading">Click for details</div>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
